8ddc962c4d15013df8c0f419ed12ada3194f7046
Ashimka <ashimka2010@yandex.ru>
2024-03-10 20:15:10 +0500

    added about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <>
       <div className="container">
-        <div className="about">About</div>
+        <div className="about">
+          <p>Я веб-разработчик Михаил Михайлов.</p>
+          <p>Технологический стек.</p>
+          <p>Backend: Node, Express, MySQL, PrismaORM, JWT.</p>
+          <p>Frontend: React, Redux ToolKit, Axios.</p>
+        </div>
+        <div className="resume">
+          Резюме на
+          <Link
+            className="about-link"
+            to={
+              "https://samara.hh.ru/resume/7cfea28fff098521a90039ed1f53436c727577"
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hh.ru
+          </Link>
+        </div>
       </div>
     </>
   );
